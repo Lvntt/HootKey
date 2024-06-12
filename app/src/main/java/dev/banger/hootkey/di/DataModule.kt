c937d976273394ffc84babe06fe5600be67ebd3e
Suslanium <zaharov22m@gmail.com>
2024-06-12 21:23:24 +0700

    Template repo di

--- app/src/main/java/dev/banger/hootkey/di/DataModule.kt
+++ app/src/main/java/dev/banger/hootkey/di/DataModule.kt
@@ -2,6 +2,7 @@ package dev.banger.hootkey.di
 
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 import dev.banger.hootkey.data.crypto.CryptoManager
 import dev.banger.hootkey.data.crypto.PasswordValidator
 import org.koin.android.ext.koin.androidApplication
@@ -10,8 +11,11 @@ import org.koin.dsl.module
 
 private fun provideFirebaseAuth() = Firebase.auth
 
+private fun provideFirebaseFireStore() = Firebase.firestore
+
 fun provideDataModule() = module {
     single { provideFirebaseAuth() }
+    single { provideFirebaseFireStore() }
     singleOf(::CryptoManager)
     single { PasswordValidator(androidApplication()) }
 }
--- app/src/main/java/dev/banger/hootkey/di/DomainModule.kt
+++ app/src/main/java/dev/banger/hootkey/di/DomainModule.kt
@@ -10,6 +10,8 @@ import dev.banger.hootkey.domain.usecase.CheckUserLoggedInUseCase
 import dev.banger.hootkey.domain.usecase.ValidateEmailUseCase
 import dev.banger.hootkey.domain.usecase.ValidatePasswordUseCase
 import dev.banger.hootkey.Constants
+import dev.banger.hootkey.data.repository.TemplateRepositoryImpl
+import dev.banger.hootkey.domain.repository.TemplateRepository
 import dev.banger.hootkey.domain.usecase.CheckPasswordUseCase
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
@@ -20,6 +22,7 @@ import org.koin.dsl.module
 
 fun provideDomainModule() = module {
     singleOf(::AuthRepositoryImpl) bind AuthRepository::class
+    singleOf(::TemplateRepositoryImpl) bind TemplateRepository::class
 
     factoryOf(::RegisterUseCase) {
         named(Constants.REGISTER)
