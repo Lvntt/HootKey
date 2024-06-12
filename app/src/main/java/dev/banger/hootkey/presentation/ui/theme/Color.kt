4adf22325db8542f8555144cc77e56a28fbd8808
Matvey Seregin <daymatt@mail.ru>
2024-06-12 14:35:50 +0700

    add colors

--- app/src/main/java/dev/banger/hootkey/presentation/ui/theme/Color.kt
+++ app/src/main/java/dev/banger/hootkey/presentation/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package dev.banger.hootkey.presentation.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryPink = Color(0xFFF2167B)
+val PrimaryOrange = Color(0xFFF86668)
+val Primary20 = Color(0xFFFFECE8)
+val Primary = Brush.linearGradient(listOf(PrimaryPink, PrimaryOrange))
+
+val Secondary = Color(0xFF171F46)
+val Secondary80 = Color(0xFF47495B)
+val Secondary60 = Color(0xFF757784)
+
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFFF0F4F7)
+val DarkGray = Color(0xFFD9D9D9)
