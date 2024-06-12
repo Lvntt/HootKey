787c3934537a7acb37dfc3431d00990b1e67685a
Matvey Seregin <daymatt@mail.ru>
2024-06-12 14:35:41 +0700

    add fonts and text styles

---
+++ app/src/main/java/dev/banger/hootkey/presentation/ui/theme/TextStyle.kt
@@ -0,0 +1,71 @@
+package dev.banger.hootkey.presentation.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val TypeB24 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp,
+    lineHeight = 31.2.sp,
+)
+
+val TypeM20 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Medium,
+    fontSize = 20.sp,
+    lineHeight = 26.sp,
+)
+
+val TypeB16 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+)
+
+val TypeM16 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+)
+
+val TypeM14 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    lineHeight = 18.2.sp,
+)
+
+val TypeR14 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    lineHeight = 18.2.sp,
+)
+
+val TypeR14_150 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    lineHeight = 21.sp,
+)
+
+val TypeB14 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+    lineHeight = 21.sp,
+)
+
+val TypeM12 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp,
+)
+
+val TypeR10 = TextStyle(
+    fontFamily = Ubuntu,
+    fontWeight = FontWeight.Normal,
+    fontSize = 10.sp,
+)
