166de887f3fce1135ff991f00801b7e6a222d543
Suslanium <zaharov22m@gmail.com>
2024-06-25 06:18:48 +0700

    Some small fixes/improvements

--- app/src/main/java/dev/banger/hootkey/presentation/ui/screen/vaults_list/VaultsListScreen.kt
+++ app/src/main/java/dev/banger/hootkey/presentation/ui/screen/vaults_list/VaultsListScreen.kt
@@ -192,7 +192,10 @@ fun VaultsListScreen(
                     VaultShortItem(modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 20.dp),
-                        iconModel = { "https://www.google.com/s2/favicons?domain=${vault.link}&sz=256" },
+                        iconModel = {
+                            "https://www.google.com/s2/favicons?domain=${vault.link}&sz=256"
+                                .takeIf { !vault.link.isNullOrBlank() }
+                        },
                         name = vault.name,
                         login = vault.login ?: "",
                         onClick = {
